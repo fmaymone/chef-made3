6a24d9adfe3300ebce442d1f3e511f7ba671dbdc
fmaymone <fmaymone@gmail.com>
2016-10-25 17:43:48 +0000

    env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-
+  config.action_mailer.default_url_options = { host: 'floating-anchorage-32035.herokuapp.com'}
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.delivery_method = :smtp
