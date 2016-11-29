50c67b4e775f64641b1ca20e8301225335434871
fmaymone <fmaymone@gmail.com>
2016-11-29 19:55:39 +0000

    menus and reviews working

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,22 +1,24 @@
 module ApplicationHelper
 	def avatar_url(user)
-		gravatar_id = Digest::MD5::hexdigest(user.email).downcase 
+		gravatar_id = Digest::MD5::hexdigest(user.email).downcase
 		if user.image
 			user.image
 		else
 			"https://www.gravatar.com/avatar/#{gravatar_id}.jpg?"
 		end
 	end
-	
+
 	 def isAdmin(user)
-         puts "66666666666666666666666666666666666666666666666666"
-         puts user.email
-         puts "66666666666666666666666666666666666666666666666666"
-         @retorno = AdminUser.where(email: user.email)
-       
-         @retorno.size > 0 ? true : false
-        
-        
-    end
-	
+		 @retorno = AdminUser.where(email: user.email)
+		 @retorno.size > 0 ? true : false
+	end
+	def avatar(menu)
+
+		if menu.documents.size > 0
+			menu.documents[0].image
+		else
+			image_url("not-available.jpg")
+		end
+	end
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ActiveRecord::Base
-  
+
   belongs_to :user
+  belongs_to :menu
   belongs_to :reviewable, polymorphic: true
 end
